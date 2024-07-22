6e5b4651ef9b1029b68e0d18acf68e6449835beb
Astha Sharma <95237199+1-Aastha@users.noreply.github.com>
2024-07-22 12:09:31 +0530

    Update 26. Remove Duplicates From Sorted Array.cpp

--- 26. Remove Duplicates From Sorted Array.cpp
+++ 26. Remove Duplicates From Sorted Array.cpp
@@ -1,3 +1,4 @@
+//1:
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
@@ -6,4 +7,26 @@ public:
             return s1.size();    
     }
 };
+
+//2:
+vector<int> v2;
+for(auto i =nums.begin();i<nums.end();i++){
+    if(binary_search(i+1,nums.end(),*i)==false){
+    v2.push_back(*i);
+    }
+}
+      nums=v2;
+      return nums.size();}
+};
+
+//3:
+int j=1;
+for(int i=1;i<nums.size();i++){//works on any type of array
+ if(nums[i]!=nums[i-1]){
+nums[j]=nums[i];
+    j++;
+            }
+            }
+            return j;
+
       
