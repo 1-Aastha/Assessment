b794fabe158d082130b2e0d159d461c947989b2b
Astha Sharma <95237199+1-Aastha@users.noreply.github.com>
2024-07-23 15:47:56 +0530

    Create Next Permutation.cpp

---
+++ Next Permutation.cpp
@@ -0,0 +1,8 @@
+//1. This is just by using next_permutation(first ieterator, last iterator) func
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        next_permutation(nums.begin(),nums.end());  //it maynot be best to use this code
+    }
+};
+
