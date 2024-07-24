1b1b9df5fff5315f267997f62453f69115b0c757
Astha Sharma <95237199+1-Aastha@users.noreply.github.com>
2024-07-24 11:35:19 +0530

    Create  Search Insert Position 1.cpp

---
+++ Search Insert Position 1.cpp
@@ -0,0 +1,23 @@
+int main() {
+    int n;
+    cin>>n;
+    int num[n];
+    for(int i=0;i<n;i++){
+        cin>>num[i];
+        
+    }
+    int target;
+    cin>>target;
+    int j=0;
+    for(;j<n;j++){
+        if(num[j]>=target){
+            break;
+        }}
+               
+                
+   
+     cout << j << endl;
+    
+    
+    return 0;
+}
