1e9c95c48ade731909e356179d77be72244addb0
Astha Sharma <95237199+1-Aastha@users.noreply.github.com>
2024-07-24 11:38:45 +0530

    Update Plus One 5.cpp

--- Plus One 5.cpp
+++ Plus One 5.cpp
@@ -16,4 +16,38 @@ public:
         return digits;
     }
 };
+
+//2:
+int main() {
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for (int i = n - 1; i >= 0; --i) {
+        
+        arr[i]++;
+        
+        
+        if (arr[i] < 10) {
+            for (int j = 0; j < n; ++j) {
+                cout << arr[j] << " ";
+            }
+            cout << endl;
+            return 0;
+        }
+        
+        
+        arr[i] = 0;
+    }
+    
+    cout << "1 ";
+    for (int i = 0; i < n; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
         
