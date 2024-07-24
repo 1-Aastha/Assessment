0bf76dde37a6252e9d33d0e7aa3d73c3f28c2e80
Astha Sharma <95237199+1-Aastha@users.noreply.github.com>
2024-07-24 11:23:59 +0530

    Create If-else challenge.cpp

---
+++ If-else challenge.cpp
@@ -0,0 +1,12 @@
+int main() {
+    int n;
+    cin>>n;
+    
+    if((n%2!=0) || (n>=6 and n<=20)){
+            cout<<"Weird"<<endl;
+        }
+        else{
+            cout<<"Not Weird"<<endl;
+        }
+    return 0;
+}
