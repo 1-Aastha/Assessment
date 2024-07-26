b423efbc61e0644d494c8ccb7e1c44f243ea6230
Astha Sharma <95237199+1-Aastha@users.noreply.github.com>
2024-07-26 11:32:37 +0530

    Update Matrix Diagonal Sum.cpp

--- Matrix Diagonal Sum.cpp
+++ Matrix Diagonal Sum.cpp
@@ -6,7 +6,7 @@ public:
         for(int i =0; i<n;i++){//iterate through each row of matrix
             sum+= mat[i][i]+ mat[i][n-i-1]; //primary diagonal+secondary diagonal
         }
-        if(n%2!=0){//checking odd condition 
+        if(n%2==1){//checking even condition 
             return sum -= mat[n/2][n/2];//subtract middle element from the sum as it has been added twice, once in primary diagonal and once in secondary diagonal
         }
         
