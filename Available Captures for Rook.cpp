cc01c06b0495f2606972dd7e71fe1628769c2c8c
Astha Sharma <95237199+1-Aastha@users.noreply.github.com>
2024-07-26 13:40:03 +0530

    Update Available Captures for Rook.cpp

--- Available Captures for Rook.cpp
+++ Available Captures for Rook.cpp
@@ -5,7 +5,7 @@ public:
         int count=0;
         int rookRow=-1;
         int rookColumn=-1;
-        for(int i=0;i<8;i++){
+        for(int i=0;i<8;i++){ //iterate throught the rows and cols of board to find the position of rook
             for(int j=0;j<8;j++){
                 if(board[i][j]=='R'){
                     rookRow=i;
@@ -14,17 +14,17 @@ public:
                 }
             }
         }
-           for(int i = rookRow - 1; i >= 0; i--) {
-        	if(board[i][rookColumn] == 'B') {
+           for(int i = rookRow - 1; i >= 0; i--) {//check rook upper
+        	if(board[i][rookColumn] == 'B') {//if bishop is encountered break through the if 
         		break;
         	}else {
-        		if(board[i][rookColumn] == 'p') {
+        		if(board[i][rookColumn] == 'p') {//if pawn is found, increase counter
         			count++;
         			break;
         		}
         	}
         }
-        for(int i = rookRow + 1; i < 8; i++) {
+        for(int i = rookRow + 1; i < 8; i++) {//check rook down
         	if(board[i][rookColumn] == 'B') {
         		break;
         	}else {
@@ -35,7 +35,7 @@ public:
         	}
         	
         }
-        for(int i = rookColumn - 1; i >= 0; i--) {
+        for(int i = rookColumn - 1; i >= 0; i--) {//check rook left
         	if(board[rookRow][i] == 'B') {
         		break;
         	}else {
@@ -45,7 +45,7 @@ public:
         		}
         	}
         }
-        for(int i = rookColumn + 1; i < 8; i++) {
+        for(int i = rookColumn + 1; i < 8; i++) {//check rook right
         	if(board[rookRow][i] == 'B') {
         		break;
         	}else {
