32702825fae12b5e651d365b429cae247e3824a3
Astha Sharma <95237199+1-Aastha@users.noreply.github.com>
2024-07-30 12:05:07 +0530

    Update and rename Delete a Node from Linked List .cpp(CodingNinja) to Delete a Node from Linked List(CodingNinja) .cpp

--- Delete a Node from Linked List(CodingNinja) .cpp
+++ Delete a Node from Linked List(CodingNinja) .cpp
@@ -15,7 +15,7 @@
         };
 
 *****************************************************************/
-
+//sol1:
 Node *deleteNode(Node *head, int pos)
 {
     if(head==NULL){
@@ -32,3 +32,34 @@ Node *deleteNode(Node *head, int pos)
 	}
 	return head;
 }
+
+//solution 2
+Node *deleteNode(Node *head, int pos)
+{
+    if(head==NULL){
+		return head;
+	}
+	else if(pos==0){
+		//Node *temp=head;
+		head=head->next;
+		//head=head->next;
+		//delete temp;
+	}
+	else {//if(pos>0){ 
+		//Node *temp=deleteNode(head->next,pos-1);
+		//head->next=temp;
+		int i=0;
+		Node *temp=head;
+		while(i<pos-1 && temp!=NULL){
+			temp=temp->next;
+			i++;
+		}
+		if(temp==NULL || temp->next==NULL)
+		return head;
+		else
+		temp->next=temp->next->next;
+		return head;
+	}
+	return head;
+}
+
