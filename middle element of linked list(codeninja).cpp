ab779ad811dcd48ba9abd0a080ce0715d808497f
Astha Sharma <95237199+1-Aastha@users.noreply.github.com>
2024-07-31 11:14:04 +0530

    Update middle element of linked list(codeninja).cpp

--- middle element of linked list(codeninja).cpp
+++ middle element of linked list(codeninja).cpp
@@ -40,3 +40,15 @@ while (mid--) { //mid decrement loop
 }
 return c;
 }
+
+//logic 2: better one 
+Node *findMiddle(Node *head) {
+    Node *p1=head;
+    Node *p2=head;
+    while (p1 != NULL && p1->next != NULL) {
+        p1 = p1->next->next;
+        p2 = p2->next;
+    }
+    return p2;
+}
+
