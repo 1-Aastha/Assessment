1b1a3da2552bc52dbe71227d4cb5091646e2b112
Astha Sharma <95237199+1-Aastha@users.noreply.github.com>
2024-07-31 11:44:40 +0530

    Create Binary Linked List To Integer (codeninja).cpp

---
+++ Binary Linked List To Integer (codeninja).cpp
@@ -0,0 +1,69 @@
+/************************************************************
+
+    Following is the linkedList class structure:
+
+    class Node {
+    public:
+        int data;
+        Node *next;
+
+        Node(int val) {
+            this->data = val;
+            next = NULL;
+        }
+        ~Node() {
+            if (next != NULL) {
+                delete next;
+            }
+        }
+};
+
+************************************************************/
+
+int binaryToInteger(int n, Node *head) {
+    int num= 0;
+    Node* p = head;
+
+    while (p != NULL) {
+        // Multiply the current decimal value by 2
+        num = num * 2;
+        // Add the current node's data (0 or 1)
+        num+=p->data;
+        // Move to the next node
+        p = p->next;
+    }
+
+    return num;
+The approach is simple, you have to take an integer to store the final answer, and iterate through the list and add the current element to “ans” and multiply ans by 2.
+
+ 
+
+The steps are as follows:
+
+ 
+
+    Take an integer “num” and initialize it with zero.
+    Iterate through the list till we reach the last element:
+        Multiply “num” by 2.
+        Update “num = “head” -> data.
+        Jump to the next node i.e. “head” = “head” -> next.
+    Return “num”.
+
+Time Complexity
+
+O(N). Where N is the number of nodes in the list.
+
+ 
+
+Since we are iterating through the list having n nodes,
+
+Hence the time complexity is O(N).
+Space Complexity
+
+O(1).
+
+ 
+
+We are not using any auxiliary space.
+
+Hence the space complexity is O(1).
